578dd6e6c2314559caa41aac29ccc2737588ccf7
stefanRitter <stefan@stefanritter.com>
2013-12-08 11:57:12 +0000

    update post comments num_likes

--- mongodb/final/final.js
+++ mongodb/final/final.js
@@ -1,4 +1,4 @@
-
+var db;
 
 // enron messages count
 
@@ -11,4 +11,15 @@ db.messages.count({ "headers.From": "andrew.fastow@enron.com", "headers.To": "jo
 
 // Please add the email address "mrpotatohead@mongodb.com" to the list of addresses in 
 // the "headers.To" array for the document with "headers.Message-ID" of 
-// "<8147308.1075851042335.JavaMail.evans@thyme>" 
+// "<8147308.1075851042335.JavaMail.evans@thyme>"
+
+db.messages.find({"headers.Message-ID": "<8147308.1075851042335.JavaMail.evans@thyme>"}).pretty()
+
+db.messages.update({"headers.Message-ID": "<8147308.1075851042335.JavaMail.evans@thyme>"},
+  {$push: {"headers.To": "mrpotatohead@mongodb.com"}});
+
+
+
+// blog update num_likes
+
+db.posts.update({permalink:"mxwnnnqaflufnqwlekfd"}, {"$inc": {"comments.0.num_likes": 1}});
