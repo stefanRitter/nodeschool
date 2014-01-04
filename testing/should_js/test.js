fd40badd0ae0ada0f195f5d1ba8f5aa59d8efd86
stefanRitter <stefan@stefanritter.com>
2014-01-04 16:21:11 +0000

    add should style testing examples

--- testing/should_js/test.js
+++ testing/should_js/test.js
@@ -1,2 +1,22 @@
+'use strict';
+
+require('should');
+
 var tips = require('./calc_tips.js'),
-    should = require('should');
+    
+    tax = 0.12,
+    tip = 0.15,
+    prices = [10, 20],
+    pricesWithTipAndTax = tips.addPercentageToEach(prices, tip + tax);
+
+pricesWithTipAndTax[0].should.equal(12.7);
+pricesWithTipAndTax[1].should.equal(25.4);
+
+var totalAmount = tips.sum(pricesWithTipAndTax).toFixed(2);
+totalAmount.should.equal('38.10');
+
+var totalAmountAsCurrency = tips.dollarFormat(totalAmount);
+totalAmountAsCurrency.should.equal('$38.10');
+
+var tipAsPercent = tips.percentFormat(tip);
+tipAsPercent.should.equal('15%');
